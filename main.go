ce81718cb3b6676327050693bd7675ba5e1af170
Kolcman <m.kolcman@yandex.ru>
2026-09-23 16:34:06 +0300

    Функция генерации пароля переделана в метод. Добавлен коструктор для объекта аккаунт.

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand/v2"
+	"net/url"
 )
 
 var alphabet = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*?-_+=")
@@ -13,17 +15,36 @@ type account struct {
 	url      string
 }
 
-func main() {
-	fmt.Println(generatePassword(10))
-	login := promptData("Введите логин: ")
-	password := promptData("Введите пароль: ")
-	url := promptData("Введите URL: ")
-
-	myAccount := account{
-		login,
-		password,
-		url,
+func (account *account) generatePassword(n int) {
+	password := make([]rune, n)
+	for i := range password {
+		password[i] = alphabet[rand.IntN(len(alphabet))]
 	}
+	account.password = string(password)
+}
+
+func newAccount(login string, password string, urlString string) (*account, error) {
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("некорректный адрес")
+	}
+	return &account{
+		login:    login,
+		password: password,
+		url:      urlString,
+	}, nil
+}
+
+func main() {
+	login := promptData("Введите логин: ")
+	urlString := promptData("Введите URL: ")
+
+	myAccount, err := newAccount(login, "", urlString)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	myAccount.generatePassword(10)
 	fmt.Print(myAccount)
 }
 
@@ -32,12 +53,4 @@ func promptData(prompt string) string {
 	var res string
 	fmt.Scan(&res)
 	return res
-}
-
-func generatePassword(n int) string {
-	password := make([]rune, n)
-	for i := range password {
-		password[i] = alphabet[rand.IntN(len(alphabet))]
-	}
-	return string(password)
 }
