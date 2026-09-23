3c2495d3e6b3d9461b2eb8e2e4a83dfec9361919
Kolcman <m.kolcman@yandex.ru>
2026-09-23 16:01:49 +0300

    Добавлена функция генерации пароля.

--- main.go
+++ main.go
@@ -1,19 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+var alphabet = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*?-_+=")
+
+type account struct {
+	login    string
+	password string
+	url      string
+}
 
 func main() {
-	login := promptData("Введите логи")
-	password := promptData("Введите логи")
-	url := promptData("Введите URL")
+	fmt.Println(generatePassword(10))
+	login := promptData("Введите логин: ")
+	password := promptData("Введите пароль: ")
+	url := promptData("Введите URL: ")
 
-	fmt.Print(login, password, url)
+	myAccount := account{
+		login,
+		password,
+		url,
+	}
+	fmt.Print(myAccount)
 }
 
 func promptData(prompt string) string {
-
 	fmt.Print(prompt)
 	var res string
 	fmt.Scan(&res)
 	return res
+}
+
+func generatePassword(n int) string {
+	password := make([]rune, n)
+	for i := range password {
+		password[i] = alphabet[rand.IntN(len(alphabet))]
+	}
+	return string(password)
 }
